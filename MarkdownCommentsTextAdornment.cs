9fa9d1deed93f5f74b00fd0f2f695b7104c69ce6
Julien Duminil <duminil@free.fr>
2015-03-15 23:38:05 +0100

    Load right images with markdown tags.

--- MarkdownCommentsTextAdornment.cs
+++ MarkdownCommentsTextAdornment.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,7 +10,7 @@ using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Formatting;
-using System.Collections.Generic;
+using System.Net;
 
 namespace MarkdownComments
 {
@@ -60,60 +62,58 @@ namespace MarkdownComments
             //grab a reference to the lines in the current TextView 
             IWpfTextViewLineCollection textViewLines = _view.TextViewLines;
 
+            Nullable<SnapshotSpan> commentSpan = null;
+
             // Loop through each comment span
             SnapshotSpan lineSpan = line.Extent;
             IList<ClassificationSpan> classificationSpanList = _classifier.GetClassificationSpans(lineSpan);
             foreach(ClassificationSpan classificationSpan in classificationSpanList)
             {
-                if(classificationSpan.ClassificationType.IsOfType("comment"))
+                //DrawMarkerGeometry(classificationSpan.Span);
+                if (classificationSpan.ClassificationType.IsOfType("comment"))
                 {
                     SnapshotSpan span = classificationSpan.Span;
                     //DrawMarkerGeometry(span);
 
-                    Regex regex = new Regex(@"\!\[([^\]]+)\]");
-                    Match match = regex.Match(span.GetText());
-                    if(match.Success)
+                    if(commentSpan != null && span.Start.Position == commentSpan.Value.End.Position)
                     {
-                        SnapshotSpan imageSpan = new SnapshotSpan(_view.TextSnapshot, Span.FromBounds(span.Start.Position + match.Index, span.Start.Position + match.Index + match.Length));
-                        DrawMarkerGeometry(imageSpan);
-
-                        Geometry g = _view.TextViewLines.GetMarkerGeometry(imageSpan);
-                        if (g != null)
+                        commentSpan = new SnapshotSpan(commentSpan.Value.Start, span.End);
+                    }
+                    else
+                    {
+                        if (commentSpan != null)
                         {
-                            Image image = new Image();
-                            BitmapFrame bitmapFrame = BitmapFrame.Create(new Uri("https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png", UriKind.RelativeOrAbsolute));
-                            image.Source = bitmapFrame;
-
-                            //Align the image with the bottom of the bounds of the text geometry
-                            Canvas.SetLeft(image, g.Bounds.Left);
-                            if (!bitmapFrame.IsDownloading)
-                                Canvas.SetTop(image, g.Bounds.Bottom - image.Source.Height);
-                            else
-                            {
-                                Canvas.SetTop(image, g.Bounds.Top);
-                                bitmapFrame.DownloadCompleted += delegate(Object sender, EventArgs e)
-                                {
-                                    Canvas.SetTop(image, g.Bounds.Bottom - image.Source.Height);
-                                };
-                            }
-
-                            _layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, imageSpan, null, image, null);
+                            ParseMarkdown(commentSpan.Value);
                         }
+
+                        commentSpan = span;
                     }
                 }
             }
 
-            ////Loop through each character, and place a box around any a 
-            //int start = line.Start;
-            //int end = line.End;
-            //for (int i = start; (i < end); ++i)
-            //{
-            //    if (_view.TextSnapshot[i] == 'a')
-            //    {
-            //        SnapshotSpan span = new SnapshotSpan(_view.TextSnapshot, Span.FromBounds(i, i + 1));
-            //        DrawMarkerGeometry(span);
-            //    }
-            //}
+            if (commentSpan != null)
+            {
+                ParseMarkdown(commentSpan.Value);
+            }
+        }
+
+        private void ParseMarkdown(SnapshotSpan span)
+        {
+            //DrawMarkerGeometry(span);
+
+            string imageAltTextRegex = @"\!\[([^\]]*)\]";
+            string imageOptTitleRegex = @"(?:\s+""([^""\)]*)"")?";
+            string imageUrlRegex = @"\(([^\s\)]+)" + imageOptTitleRegex + @"\)";
+            Regex imageRegex = new Regex(imageAltTextRegex + imageUrlRegex);
+            foreach (Match match in imageRegex.Matches(span.GetText()))
+            {
+                SnapshotSpan imageSpan = new SnapshotSpan(_view.TextSnapshot, Span.FromBounds(span.Start.Position + match.Index, span.Start.Position + match.Index + match.Length));
+                string altText = match.Groups[1].Value;
+                string uri = match.Groups[2].Value;
+                string optTitle = match.Groups[3].Value;
+                DrawMarkerGeometry(imageSpan);
+                DrawImage(imageSpan, altText, uri, optTitle);
+            }
         }
 
         private void DrawMarkerGeometry(SnapshotSpan span)
@@ -135,7 +135,68 @@ namespace MarkdownComments
                 Canvas.SetLeft(image, g.Bounds.Left);
                 Canvas.SetTop(image, g.Bounds.Top);
 
-                _layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, span, null, image, null);
+                _layer.AddAdornment(span, null, image);
+            }
+        }
+
+        private void DrawImage(SnapshotSpan span, string altText, string uri, string optTitle)
+        {
+            Geometry g = _view.TextViewLines.GetMarkerGeometry(span);
+            if (g != null)
+            {
+                Image image = new Image();
+
+#if false
+                BitmapImage imageSource = new BitmapImage();
+                using (FileStream stream = File.OpenRead("samples\\icon48.png"))
+                //WebRequest req = HttpWebRequest.Create("https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png");
+                //using (Stream stream = req.GetResponse().GetResponseStream())
+                //using (Stream stream = Application.GetResourceStream(new Uri("https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png", UriKind.Absolute)).Stream)
+                {
+                    imageSource.BeginInit();
+                    imageSource.StreamSource = stream;
+                    imageSource.CacheOption = BitmapCacheOption.OnLoad;
+                    imageSource.EndInit(); // load the image from the stream
+                } // close the stream
+#else
+                BitmapFrame imageSource = null;
+                try
+                {
+                    imageSource = BitmapFrame.Create(new Uri(uri, UriKind.RelativeOrAbsolute));
+                }
+                catch (FileNotFoundException e)
+                {
+                    // TODO: set alt text instead?
+                }
+#endif
+                image.Source = imageSource;
+
+                if(optTitle != "")
+                {
+                    //ToolTip tt = new ToolTip();
+                    //tt.Content = optTitle;
+                    //image.ToolTip = tt;
+                    image.ToolTip = optTitle;
+                }
+
+                //Align the image with the bottom of the bounds of the text geometry
+                Canvas.SetLeft(image, g.Bounds.Left);
+                if (!imageSource.IsDownloading)
+                    Canvas.SetTop(image, g.Bounds.Bottom - image.Source.Height);
+                else
+                {
+                    Canvas.SetTop(image, g.Bounds.Top);
+                    imageSource.DownloadCompleted += delegate(Object sender, EventArgs e)
+                    {
+                        Canvas.SetTop(image, g.Bounds.Bottom - image.Source.Height);
+                    };
+                    imageSource.DownloadFailed += delegate(Object sender, ExceptionEventArgs e)
+                    {
+                        // TODO: set alt text instead?
+                    };
+                }
+
+                _layer.AddAdornment(span, null, image);
             }
         }
     }
