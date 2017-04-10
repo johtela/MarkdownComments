3f59586de2ae1b8a78731a9fb5495c88432d2d34
johtela <tommi.johtela@dnainternet.net>
2017-04-11 00:16:04 +0300

    Changed the parser regex to work with multiple headers inside a comment block.

---
+++ Backup/samples/sample.cpp
@@ -0,0 +1,25 @@
+
+// # myClass section
+
+class myClass
+{
+public:
+	// ## Public methods
+
+	void myFunc1() {} // Emphasis, aka italics, with *asterisks* or _underscores_.
+	void myFunc2() {} // Strong emphasis, aka bold, with **asterisks** or __underscores__.
+	void myFunc3() {} // Combined emphasis with **asterisks and _underscores_**.
+	void myFunc4() {} // Strikethrough uses two tildes. ~~Scratch this.~~
+
+	/*
+		Some image ![Markdown logo](Markdown-mark.png "Markdown") here
+		Also works with URL: ![Markdown logo](http://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/64px-Markdown-mark.svg.png "Markdown")
+		An animated Gif: ![Animated gif](https://upload.wikimedia.org/wikipedia/commons/d/d3/Newtons_cradle_animation_book_2.gif "Newton's cradle")
+		Image with error: ![image with error](donotexists.png "fake")
+	*/
+
+private:
+	// ## Private members
+
+	int m_myInt = 0; // contains some **useful** value
+};
